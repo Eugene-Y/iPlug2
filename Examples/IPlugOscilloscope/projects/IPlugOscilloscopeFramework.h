42b8eca5f472b19b0870182cdc65f4b79fdf28b1
Eugene Y <oneeqinfinity@yandex.ru>
2019-03-03 16:58:37 +0300

    duplicated IPlugEffect

---
+++ Examples/IPlugOscilloscope/projects/IPlugOscilloscopeFramework.h
@@ -0,0 +1,13 @@
+@import UIKit;
+
+//! Project version number for IPlugOscilloscopeFramework.
+FOUNDATION_EXPORT double IPlugOscilloscopeFramework_VersionNumber;
+
+//! Project version string for IPlugOscilloscopeFramework.
+FOUNDATION_EXPORT const unsigned char IPlugOscilloscopeFramework_VersionString[];
+
+#import <IPlugOscilloscopeFramework/config.h>
+#import <IPlugOscilloscopeFramework/IPlugAUAudioUnit.h>
+
+@class IPlugOscilloscopeViewController;
+
